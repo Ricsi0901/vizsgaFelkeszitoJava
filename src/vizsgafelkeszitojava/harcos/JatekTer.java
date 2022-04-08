a320aa04d895da1f023752061a76e15416021b17
ricsi0901 <ricsi.szucs22@gmail.com>
2022-04-08 14:19:08 +0200

    mappák létrehozása

---
+++ src/vizsgafelkeszitojava/harcos/JatekTer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vizsgafelkeszitojava.harcos;
+
+/**
+ *
+ * @author Szucs.R.David
+ */
+public class JatekTer {
+    public static void main(String[] args) {
+        
+    }
+}
